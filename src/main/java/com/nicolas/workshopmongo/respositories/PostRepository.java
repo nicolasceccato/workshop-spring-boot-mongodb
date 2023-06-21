a7bc9a79d35a1e1000c381cb83b9d39b38cca835
nicolasceccato <nicolasceccato@hotmail.com>
2023-06-21 14:10:39 -0300

    consulta por título do post via @query

--- src/main/java/com/nicolas/workshopmongo/respositories/PostRepository.java
+++ src/main/java/com/nicolas/workshopmongo/respositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.nicolas.workshopmongo.respositories;
 import com.nicolas.workshopmongo.domain.Post;
 import com.nicolas.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +12,8 @@ import java.util.List;
 public interface PostRepository extends MongoRepository<Post, String> {
 
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ 'title': { $regex: ?0, $options:  'i'} }")
+    List<Post> searchTitle(String text);
+
 }
