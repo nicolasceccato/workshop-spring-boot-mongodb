1422e35abaca4910a8907d6cc2962ceae1863c6c
nicolasceccato <nicolasceccato@hotmail.com>
2023-06-21 14:41:11 -0300

    consulta com vários critérios

--- src/main/java/com/nicolas/workshopmongo/respositories/PostRepository.java
+++ src/main/java/com/nicolas/workshopmongo/respositories/PostRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -15,5 +16,8 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
     @Query("{ 'title': { $regex: ?0, $options:  'i'} }")
     List<Post> searchTitle(String text);
+
+    @Query("{ $and:  [ {date: {$gte:  ?1}}, {date: {$lte:  ?2}}, { $or: [ { 'title': { $regex: ?0, $options:  'i'} }, { 'body': { $regex: ?0, $options:  'i'} }, { 'comments.text': { $regex: ?0, $options:  'i'} }] }]}")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 
 }
--- src/main/java/com/nicolas/workshopmongo/services/PostService.java
+++ src/main/java/com/nicolas/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.nicolas.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,5 +26,8 @@ public class PostService {
         return repo.searchTitle(text);
     }
 
-
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repo.fullSearch(text, minDate, maxDate);
+    }
 }
