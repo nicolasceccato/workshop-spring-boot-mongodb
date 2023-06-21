4f0e224d53b8a71378a0de11d2ea7f7bada944b1
nicolasceccato <nicolasceccato@hotmail.com>
2023-06-20 21:14:13 -0300

    atualizando usuários

--- src/main/java/com/nicolas/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nicolas/workshopmongo/resources/UserResource.java
@@ -46,5 +46,13 @@ public class UserResource {
         return ResponseEntity.noContent().build();
     }
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDTO, @PathVariable String id) {
+        User obj = service.fromDTO(objDTO);
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
--- src/main/java/com/nicolas/workshopmongo/services/UserService.java
+++ src/main/java/com/nicolas/workshopmongo/services/UserService.java
@@ -34,6 +34,18 @@ public class UserService {
         repo.deleteById(id);
     }
 
+    public User update(User obj) {
+        User newObj = repo.findById(obj.getId()).get();
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+
+    }
+
     public User fromDTO(UserDTO objDTO) {
         return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
