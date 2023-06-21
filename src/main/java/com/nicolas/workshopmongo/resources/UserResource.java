8192653fb3e933a194552b9746e40c2452b03e53
nicolasceccato <nicolasceccato@hotmail.com>
2023-06-21 08:47:49 -0300

    endpoint para retornar os posts de um usuário

--- src/main/java/com/nicolas/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nicolas/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.nicolas.workshopmongo.resources;
 
+import com.nicolas.workshopmongo.domain.Post;
 import com.nicolas.workshopmongo.domain.User;
 import com.nicolas.workshopmongo.dto.UserDTO;
 import com.nicolas.workshopmongo.services.UserService;
@@ -52,6 +53,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body((obj.getPosts()));
     }
 
 
